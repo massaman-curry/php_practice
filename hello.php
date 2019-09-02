297016668435bcb9b46aa21f3557a03814e9b64a
massaman_curry <m.m.elcapitan@gmail.com>
2019-09-02 12:24:58 +0900

    This is an initial commit of the repository.
    You can register your data to MariaDB(MySQL) from html form, in this state. This function is realized by using PDO.
    
    As you can see, we have 3 files(form.html, hello.php, view.php), but 'hello.php' has no roll to implement the function written above.

---
+++ hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Test HTML</title>
+</head>
+
+<body>
+    <?php 
+
+    $user = 'ゲスト';
+    
+    print "$user さん, こんにちは";
+    
+    ?>
+</body>
+
+</html>
