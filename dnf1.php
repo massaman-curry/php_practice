fd3d704f948319b90a676dfe475d0dac6e01ad3f
massaman curry <m.m.elcapitan@gmail.com>
2019-12-29 01:27:03 +0900

    add comments to point out issues

--- dnf1.php
+++ dnf1.php
@@ -5,53 +5,55 @@ const GRAY = 1;
 const BLACK = 2;
 
 //$M[]が隣接行列。以下ですべてを0で作成。
-fscanf("%d", $n);
+fscanf(STDIN, "%d", $n);
+
 for($i = 0; $i < $n; $i++){
-    for($j = 0; $j < $n; $j++) $M[$i][$j] = 0;
+    for($j = 0; $j < $n; $j++) $M[$i][$j] = 0; //0~$n-1というkey
 }
 
 for($i = 0; $i < $n; $i ++){
 
-    fscanf("%d %d", $u, $k);
-
+    fscanf(STDIN, "%d %d", $u, $k);
+    $u--;
+//おそらく、fscanfは一行ごとに読んでいるから、上のfscanfでまとめて$vも取得しないと行けない気がする。explodeを使うようかも？
     for($j = 0; $j < $k; $j++){
-        fscanf("%d", $v);
-    }
-
-
-}
-
-
-
-
-function dfs_visit($u){
-
-    $colour[$u] = GRAY;
-    $d[$u] = ++$t;
-
-    for($v = 0; $v < $n; $v++){
-        if($adj_list[$u]{$v} == 0) continue; //おそらく、$uが縦、$vが横になるはず
-
-        if($colour[$v] == WHITE) dfs_visit($v);
-    }
-
-    $colour[$u] == BLACK;
-    $fin_t[$u] == ++$t;
-
-}
-
-function dfs(){
-
-    for($u = 0; $u < $n; $u++) $colour[$u] == WHITE;
-    $t = 0;
-
-    for($u = 0; $u < $n; $u++){
-        if($colour[$u] == WHITE) dfs_visit($u); 
-    }
-
-    for($u = 0; $u < $n; $u++){
-        echo $u+1, $d[$u], $t[$u];
+        fscanf(STDIN, "%d", $v);
+        $v--; //$u--と同様、一番始めに作成した$n*$n全て0の隣接行列のキーに合わせるため。
+        //デクリメントしないと、0~6の配列に1~6の配列のデータを挿入することになる。
+        $M[$u][$v] = 1;
     }
 
 }
+
+
+// function dfs_visit($u){
+
+//     $colour[$u] = GRAY;
+//     $d[$u] = ++$t;
+
+//     for($v = 0; $v < $n; $v++){
+//         if($adj_list[$u]{$v} == 0) continue; //おそらく、$uが縦、$vが横になるはず
+
+//         if($colour[$v] == WHITE) dfs_visit($v);
+//     }
+
+//     $colour[$u] == BLACK;
+//     $f[$u] == ++$t;
+
+// }
+
+// function dfs(){
+
+//     for($u = 0; $u < $n; $u++) $colour[$u] == WHITE;
+//     $t = 0;
+
+//     for($u = 0; $u < $n; $u++){
+//         if($colour[$u] == WHITE) dfs_visit($u); 
+//     }
+
+//     for($u = 0; $u < $n; $u++){
+//         echo $u+1, $d[$u], $f[$u];
+//     }
+
+// }
 
